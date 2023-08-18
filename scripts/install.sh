3fc44bc54788d4b1ab5dd1ed49442c872788ecb2
Khai Nguyen <khai.nguyen@ilotusland.com>
2023-08-18 19:46:25 +0700

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,16 +9,15 @@ print_step() {
     echo -e "$BGreen-----------------------------------------------$Color_Off"
 }
 
-
 print_step "Migrate configuration files"
 source ./scripts/migrate.sh
 
 print_step "Install Nala package manager"
-apt-get update
-apt -y install nala
+sudo apt-get update
+sudo apt -y install nala
 
 print_step "Install essensial dependencies"
-nala install -y zsh curl xclip silversearcher-ag tmux git make wget
+sudo nala install -y zsh curl xclip silversearcher-ag tmux git make wget
 
 print_step "Install Node version manager (nvm)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
@@ -43,3 +42,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 print_step "Install zsh-autosuggestions plugin"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+print_step "Install ibus-bamboo"
+sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
+sudo nala update
+sudo nala install ibus ibus-bamboo --install-recommends
+ibus restart
+env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
