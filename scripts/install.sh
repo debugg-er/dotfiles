bffd32f0020ed8f696aecc62ef6ce21db7ea1271
Khai Nguyen <khainguyen9209@gmail.com>
2023-10-30 21:04:20 +0700

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,9 +10,6 @@ print_step() {
     echo -e "$BGreen-----------------------------------------------$Color_Off"
 }
 
-# print_step "Install Nala package manager"
-# sudo apt -y install nala
-
 print_step "Install installation tools"
 sudo apt update
 sudo apt install -y software-properties-common build-essential
@@ -20,40 +17,20 @@ sudo apt install -y software-properties-common build-essential
 print_step "Setting up apt repositories"
 cat $DOTFILES_DIR/scripts/stuff/apt-repos | while read repo; do sudo add-apt-repository -y $repo; done
 
-print_step "Install programs"
+print_step "Install apt packages"
 sudo apt install -y $(cat $DOTFILES_DIR/scripts/stuff/apt-packages)
 
-print_step "Install Node version manager (nvm)"
+print_step "Install nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
-# [ -s "~/.nvm/nvm.sh" ] && \. "~/.nvm/nvm.sh"
-
-# print_step "Install lastest NodeJS"
-# nvm install --lts
-
-# print_step "Install global node's modules"
-# npm install -g yarn
-# yarn global add neovim eslint
-
-# print_step "Install alacritty"
-# sudo add-apt-repository ppa:aslatter/ppa -y
-# sudo apt install alacritty -y
-
-# print_step "Install alacritty theme"
-# mkdir -p ~/.config/alacritty/themes
-# git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
 
 print_step "Install tpm"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# print_step "Install fzf"
-# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-# yes | ~/.fzf/install
-
 print_step "Install oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 
-# print_step "Install zsh-autosuggestions plugin"
-# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+print_step "Install docker"
+curl https://get.docker.com | bash -
 
 print_step "Download kubectl"
 mkdir -p ~/.local/bin
@@ -69,11 +46,8 @@ cd .. && rm -rf keyd
 keyd reload
 
 print_step "Config ibus-bamboo"
-# add-apt-repository ppa:bamboo-engine/ibus-bamboo -y
-# sudo apt update
-# sudo apt install -y ibus ibus-bamboo --install-recommends
 ibus restart
 env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
 
-print_step "Install Snap Applications"
+print_step "Install snap softwares"
 cat $DOTFILES_DIR/scripts/stuff/snap-softwares | while read repo; do sudo snap install $repo; done
