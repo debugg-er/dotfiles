3a37e05cea52ac617c40fc8e8d67720c3c2a51b9
debugg-er <khainguyen9209@gmail.com>
2024-07-29 19:32:56 +0700

    update

--- windows/install.sh
+++ windows/install.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-sudo pacman -S --noconfirm - < ./.pacman.programs
+DOTFILES_DIR=$HOME/dotfiles
+
+sudo pacman -S --noconfirm - < $DOTFILES_DIR/windows/.pacman.programs
 
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
@@ -13,7 +15,7 @@ rm -rf yay-bin
 yay -S --answerclean None --answerdiff None fnm-bin
 
 print_step "Install tpm"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 
 print_step "Install oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
