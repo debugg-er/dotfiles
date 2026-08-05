11e4469e09fc12de28e6139a91e2da22bdffa2cf
Khai Nguyen <khainguyen9209@gmail.com>
2026-08-05 19:27:04 +0700

    update

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,3 +5,25 @@ require("core.plugin")
 require("core.mapping")
 require("core.theme")
 require("core.event")
+
+vim.opt.termguicolors = true
+
+vim.diagnostic.config({
+    underline = true,
+})
+
+local function enable_diagnostic_undercurl()
+    for _, severity in ipairs({ "Error", "Warn", "Info", "Hint" }) do
+        vim.api.nvim_set_hl(0, "DiagnosticUnderline" .. severity, {
+            undercurl = true,
+            underline = false,
+            update = true,
+        })
+    end
+end
+
+enable_diagnostic_undercurl()
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+    callback = enable_diagnostic_undercurl,
+})
